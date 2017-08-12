b19851ce9e68d37c9f5b592d5116235a595c9088
Mark Waite <mark.earl.waite@gmail.com>
2017-08-11 19:11:01 -0600

    Add HelloWorldBuilder integration test

---
+++ src/test/java/org/jenkinsci/plugins/worldintro/HelloWorldBuilderIT.java
@@ -0,0 +1,74 @@
+package org.jenkinsci.plugins.worldintro;
+
+import hudson.FilePath;
+import hudson.Launcher;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import hudson.tasks.BuildStepDescriptor;
+import static org.hamcrest.CoreMatchers.is;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.WithoutJenkins;
+
+public class HelloWorldBuilderIT {
+
+    @Rule
+    public JenkinsRule jenkins = new JenkinsRule();
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    public HelloWorldBuilderIT() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    private HelloWorldBuilder builder = null;
+    private String name = null;
+
+    @Before
+    public void setUp() {
+        name = "New name";
+        builder = new HelloWorldBuilder(name);
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    @WithoutJenkins
+    public void testGetName() {
+        assertThat(builder.getName(), is(name));
+    }
+
+    @Test
+    public void testPerform() {
+        Run build = null;
+        FilePath workspace = null;
+        Launcher launcher = null;
+        TaskListener listener = null;
+        thrown.expect(NullPointerException.class);
+        builder.perform(build, workspace, launcher, listener);
+    }
+
+    @Test
+    public void testGetDescriptor() {
+        assertThat(builder.getDescriptor(), IsInstanceOf.instanceOf(BuildStepDescriptor.class));
+    }
+
+}
